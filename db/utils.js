7f103f2e86c006b9de27bc2bf5b72cb0156da009
brian <brian.aguilar235@gmail.com>
2024-03-11 23:05:10 -0700

    updates

--- db/utils.js
+++
@@ -1,8 +0,0 @@
-const util = require('util');
-const fs = require('fs');
-
-
-const uuidv1 = require('uuid/v1');
-
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
