34e79fb0e72d452d5fdc9d3bb07be4c990de1fc2
brian <brian.aguilar235@gmail.com>
2024-03-11 23:08:05 -0700

    utils

--- db/utils.js
+++ db/utils.js
@@ -35,6 +35,29 @@ class Util {
 
 
 
+      addNote(note) {
+        const { title, text } = note;
+    
+        if (!title || !text) {
+          throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+    
+       
+        const newNote = { title, text, id: uuidv1() };
+    
+
+        return this.getNotes()
+          .then((notes) => [...notes, newNote])
+          .then((updatedNotes) => this.write(updatedNotes))
+          .then(() => newNote);
+      }
+    
+      removeNote(id) {
+      
+        return this.getNotes()
+          .then((notes) => notes.filter((note) => note.id !== id))
+          .then((filteredNotes) => this.write(filteredNotes));
+      }
 }
 
 
